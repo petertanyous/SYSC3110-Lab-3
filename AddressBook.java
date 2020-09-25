752862b6eb6d6985b21d9b77e0ad1021dca8054f
petertanyous <petertanyous@cmail.carleton.ca>
2020-09-25 01:51:53 -0400

    Changed main method added and removed buddy

--- AddressBook.java
+++ AddressBook.java
@@ -18,7 +18,10 @@ public class AddressBook {
         }
     }
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom") ;
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
 
